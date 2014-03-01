3c6f5c72170d11706adceaf7c278f94311f912c2
Kevin Lacker <lacker@gmail.com>
2014-03-01 14:37:56 -0800

    try mapping stuff into GPU memory

--- core/datasets.py
+++ core/datasets.py
@@ -2,7 +2,9 @@
 
 import cPickle
 import gzip
+import numpy
 import os
+import theano
 import urllib
 
 # Contains pickled Theano data for digit recognition
@@ -21,10 +23,50 @@ def data_path(source):
   return answer
 
 """
-Returns the MNIST image processing data
+Turns array data into a Theano shared variable so that Theano can
+control its storage location and put it on the GPU.
+Don't mutate the return value because its dependency guarantees are
+unclear.
+"""
+def make_shared(data):
+  assert data.dtype == theano.config.floatX
+  return theano.shared(numpy.asarray(data), borrow=True)
+
+"""
+Turns int array data into a variable achieved by a cast from an
+underlying float array so that the underlying data can be stored on
+the GPU.
+Don't mutate the return value.
+"""
+def make_int_shared(data):
+  assert data.dtype == "int32"
+  float_data = numpy.asarray(data, dtype=theano.config.floatX)
+  shared = theano.shared(float_data, borrow=True)
+  return T.cast(shared, "int32")
+
+"""
+Returns the MNIST image processing data in normal arrays.
+"""
+def unshared_mnist():
+  f = gzip.open(data_path(MNIST), 'rb')
+  answer = cPickle.load(f)
+  f.close()
+  return answer
+  
+"""
+Returns the MNIST image processing data in shared memory.
 """
 def mnist():
-  f = gzip.open(data_path(MNIST), 'rb')
-  train, valid, test = cPickle.load(f)
-  f.close()
-  return train, valid, test
+  ((train_input, train_output),
+   (check_input, check_output),
+   (test_input, test_output)) = unshared_mnist()
+
+  print "mapping training data"
+  s_train_input = make_shared(train_input)
+  s_train_output = make_int_shared(train_output)
+  print "mapping validation data"
+  s_check_input = make_shared(check_input)
+  s_check_output = make_int_shared(check_output)
+  print "mapping testing data"
+  s_test_input = make_shared(test_input)
+  s_test_output = make_int_shared(test_output)
