b766780b0463fd28321f889b719c2520892a985a
Kevin Lacker <lacker@gmail.com>
2014-03-02 21:54:11 -0800

    formula for neg log like

--- core/linear_classifier.py
+++ core/linear_classifier.py
@@ -31,19 +31,33 @@ class LinearClassifier(object):
     self.b = theano.shared(value=init_b, name="b")
 
     # x will be provided anew for each batch to train, so we can leave
-    # it symbolic.
-    # TODO: figure out why we do or do not need x's dimensions here
+    # it symbolic. It should have dimension batch size * input dimension
     self.x = T.matrix("x")
 
     # Predict categories with a linear transform plus max. The softmax
     # is just for the purposes of gradient descent.
     self.y = T.dot(self.x, self.W) + self.b
 
-    # y_prob is the probability of each category
+    # y_prob is the probability predicted for each category
     self.y_prob = T.nnet.softmax(self.y)
 
     # y_pred is a 1 in the predicted category, 0 in others
     self.y_pred = T.argmax(self.y_prob, axis=1)
+
+  """
+  A formula for the loss function which we are trying to minimize,
+  given the target correct classification.
+  The target should be an array of length batch size, since each
+  member of the batch has one correct classification.
+  Uses the mean instead of sum n.l.l. to be more consistent for
+  different batch sizes.
+  """
+  def negative_log_likelihood(self, target):
+    log_probs = T.log(self.y_prob)
+    # Select out the probabilities that correspond to the target
+    # categories
+    target_log_probs = log_probs[T.arange(target.shape[0]), target]
+    return -T.mean(target_log_probs)
     
     
 if __name__ == "__main__":
