61c82aa1b06a4106f2259a792034d562d0b32c92
Kevin Lacker <lacker@gmail.com>
2014-03-09 17:34:45 -0700

    one level of tree search on top of monte carlo

--- hex/players.py
+++ hex/players.py
@@ -63,6 +63,58 @@ def montecarlo(b):
     score)
   return spot
 
+
+"""
+Does a tree on top of monte carlo where the tree is just a single
+step - one move, then the responder move.
+"""
+def shallow_tree(b):
+  empty = b.empty_spots()
+  mover = b.to_move
+  # Record maps (our spot, their spot) to the win/loss record.
+  record = {}
+
+  # Run the playouts
+  playouts = 2000
+  for n in range(playouts):
+    random.shuffle(empty)
+    copy = b.copy()
+    for spot in empty:
+      copy.move(spot, check_for_winner=False)
+    winner = copy.winner()
+    if winner == mover:
+      dwin, dloss = 1, 0
+    else:
+      dwin, dloss = 0, 1
+    our_spots = []
+    their_spots = []
+    for i, j in empty:
+      if copy.board[i][j] == mover:
+        our_spots.append((i, j))
+      else:
+        their_spots.append((i, j))
+    for our_spot in our_spots:
+      for their_spot in their_spots:
+        key = (our_spot, their_spot)
+        w, l = record.get(key, (0, 0))
+        record[key] = (w + dwin, l + dloss)
+        
+  # Score each of our moves against the worst-performing response
+  ranked = []
+  for our_spot in empty:
+    worst_score = 1.0
+    for their_spot in empty:
+      if our_spot == their_spot:
+        continue
+      w, l = record.get((our_spot, their_spot), (0, 0))
+      possible_score = (1.0 + w) / (1.0 + w + l)
+      worst_score = min(worst_score, possible_score)
+    ranked.append((worst_score, our_spot))
+
+  # Pick our best move
+  score, spot = max(ranked)
+  print "best move: %s. score: %.2f" % (spot, score)
+  return spot
   
 """
 Treats a human's clicks as playing for this color whenever it is the
@@ -90,9 +142,9 @@ if __name__ == "__main__":
   b = board.Board()
   v = viewer.Viewer(b)
 
-  make_computer(montecarlo, board.BLACK, b)
+  make_computer(shallow_tree, board.BLACK, b)
 
-  make_computer(partcrazy, board.WHITE, b)
-  # make_human(v, board.WHITE, b)
+  # make_computer(montecarlo, board.WHITE, b)
+  make_human(v, board.WHITE, b)
 
-  v.root.after_idle(lambda: b.move((0, 1)))
+  v.root.after_idle(lambda: b.move((3, 3)))
