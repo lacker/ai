47291ab9928f21b7096175f9812db5054492cd0e
Kevin Lacker <lacker@gmail.com>
2014-06-01 15:57:04 -0700

    porting some basic typedeffy stuff to go

--- go/hex_board.go
+++ go/hex_board.go
@@ -4,6 +4,32 @@ import (
 	"fmt"
 )
 
+const BoardSize = 11;
+
+/*
+The board is a grid. Each value is either BLACK, WHITE, or EMPTY.
+In the external world, spots are typically referred to with a
+letter plus a number.
+Display would look like a diamond, e.g. for a 4x4 board:
+
+ABCD
+-----
+\    \       1
+ \    \      2
+  \    \     3
+   \    \    4
+    -----
+
+To represent a spot, we do row and column like it's a matrix.
+Black goes top to bottom; White goes left to right.
+So Black could win with a single column; White could win with a single row.
+*/
+
+// TODO make a Color enum
+type Spot struct {
+	Row, Col byte
+}
+
 func main() {
 	fmt.Printf("sup\n");
 }
