e5e2ddce67e690ee1338708221438979b574a943
Kevin Lacker <lacker@gmail.com>
2014-06-15 14:14:06 -0700

    more mcts bits

--- go/src/lacker.info/hex/monte_carlo_tree_search.go
+++ go/src/lacker.info/hex/monte_carlo_tree_search.go
@@ -4,6 +4,9 @@ package hex
 Monte Carlo Tree Search.
 */
 
+import (
+)
+
 type TreeNode struct {
 	BlackWins int
 	WhiteWins int
@@ -19,10 +22,17 @@ func NewRoot(b *Board) *TreeNode {
 	return node
 }
 
+// The UCT formula for how promising this node is to investigate
+func (n *TreeNode) UCT() float64 {
+	panic("TODO: implement")
+}
+
 
 type MonteCarloTreeSearch struct {
+	Root *TreeNode
 }
 
 func (mcts MonteCarloTreeSearch) Play(b *Board) Spot {
+	mcts.Root = NewRoot(b)
 	panic("TODO: implement mcts algorithm")
 }
