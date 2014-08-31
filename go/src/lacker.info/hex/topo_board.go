7f55db3284d3e6fe35c497dc2ba48a8763dba896
Kevin Lacker <lacker@gmail.com>
2014-08-30 19:43:26 -0700

    initial description of TopoBoard

---
+++ go/src/lacker.info/hex/topo_board.go
@@ -0,0 +1,13 @@
+package hex
+
+/*
+TopoBoard is a Board implementation that constantly tracks which
+groups of spots are connected. This makes it quick to determine when a
+game is over.
+*/
+
+type TopoBoard struct {
+
+}
+
+
