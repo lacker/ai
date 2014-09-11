bc194148586c85e9ad057dc8aa325185ca53f382
Kevin Lacker <lacker@gmail.com>
2014-09-10 22:36:25 -0700

    making slices of scored spots sortable

--- go/src/lacker.info/hex/spot_ranker.go
+++ go/src/lacker.info/hex/spot_ranker.go
@@ -20,6 +20,23 @@ type ScoredSpot struct {
 	Spot TopoSpot
 }
 
+// Make scored spot slices sortable
+type ScoredSpotSlice []*ScoredSpot
+func (slice ScoredSpotSlice) Len() int {
+	return len(slice)
+}
+
+func (slice ScoredSpotSlice) Less(i, j int) bool {
+	// Use > to implement < to get reverse sorting
+	// This is because we want the highest scoring stuff first
+	return slice[i].Score > slice[j].Score;
+}
+
+func (slice ScoredSpotSlice) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+// The player
 type SpotRanker struct {
 	Seconds float64
 	Quiet bool
