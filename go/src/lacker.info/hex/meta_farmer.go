9b4fac1ba0c5717446f0fb1198b9715a3e23f8fb
Kevin Lacker <lacker@gmail.com>
2014-09-22 20:30:29 -0700

    add logic to determine the best move

--- go/src/lacker.info/hex/meta_farmer.go
+++ go/src/lacker.info/hex/meta_farmer.go
@@ -20,24 +20,26 @@ type MetaFarmer struct {
 
 	whiteWinRate float64
 	blackWinRate float64
+	gamesPlayed int
 }
 
 func (mf *MetaFarmer) init(b *TopoBoard) {
-	// Initialize the metafarmer
 	mf.whitePlayer = MakeQuickPlayer(b, White)
 	mf.blackPlayer = MakeQuickPlayer(b, Black)
 	mf.whiteWinRate = 0.5
 	mf.blackWinRate = 0.5
+	mf.gamesPlayed = 0
 }
 
 func (mf *MetaFarmer) updateWinRate(winner Color) {
+	mf.gamesPlayed++
 	if winner == White {
-		mf.whiteWinRate += 0.01
+		mf.whiteWinRate += 0.001
 	} else {
-		mf.blackWinRate += 0.01
+		mf.blackWinRate += 0.001
 	}
-	mf.whiteWinRate /= 1.01
-	mf.blackWinRate /= 1.01
+	mf.whiteWinRate /= 1.001
+	mf.blackWinRate /= 1.001
 }
 
 
@@ -47,12 +49,28 @@ func (mf *MetaFarmer) Play(b Board) (Spot, float64) {
 
 	for SecondsSince(start) < mf.Seconds {
 		// Play a game
+		ending := mf.whitePlayer.Playout(mf.blackPlayer)
+		mf.updateWinRate(ending.Winner)
+
 		// Have the loser learn
+		if ending.Winner == White {
+			mf.whitePlayer.Learn(ending)
+		} else {
+			mf.blackPlayer.Learn(ending)
+		}
 	}
 
 	if !mf.Quiet {
-		log.Printf("TODO: print something useful here\n")
+		log.Printf("played %d games. white: %.2f black: %.2f\n",
+			mf.gamesPlayed, mf.whiteWinRate, mf.blackWinRate)
 	}
 
-	panic("TODO")
+	switch b.GetToMove() {
+	case White:
+		return mf.whitePlayer.ranking[0].Spot.ToSpot(), mf.whiteWinRate
+	case Black:
+		return mf.blackPlayer.ranking[0].Spot.ToSpot(), mf.blackWinRate
+	default:
+		panic("there is no player to move")
+	}
 }
