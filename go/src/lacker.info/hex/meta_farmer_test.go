3cd9b753ddaff468ea986e3b1df37a5f5ee2e9c3
Kevin Lacker <lacker@gmail.com>
2014-10-25 06:52:26 -0700

    repro the problem in a test

--- go/src/lacker.info/hex/meta_farmer_test.go
+++ go/src/lacker.info/hex/meta_farmer_test.go
@@ -1,6 +1,7 @@
 package hex
 
 import (
+	"log"
 	"testing"
 )
 
@@ -8,4 +9,18 @@ func TestMetaFarmerInit(t *testing.T) {
 	board := NewTopoBoard()
 	mf := &MetaFarmer{Seconds:-1, Quiet:true}
 	mf.init(board)
+}
+
+func TestMetaFarmerIntegration(t *testing.T) {
+	board := PuzzleMap["doomed1"].Board.ToTopoBoard()
+	mf := &MetaFarmer{Seconds:-1, Quiet:true}
+	mf.init(board)
+	mf.PlayOneCycle(false)
+	mf.PlayOneCycle(false)
+	mf.PlayOneCycle(false)
+	mf.PlayOneCycle(false)
+	realMainLine := Playout(mf.whitePlayer, mf.blackPlayer, false)
+	if mf.mainLine.Winner != realMainLine.Winner {
+		log.Fatal("main line got corrupted")
+	}
 }
