3ab89136e69ab4d32be614efbe175235ec0178c4
Kevin Lacker <lacker@gmail.com>
2014-11-13 21:19:07 -0800

    add BasicFeature

---
+++ go/src/lacker.info/hex/basic_feature.go
@@ -0,0 +1,12 @@
+package hex
+
+import (
+
+)
+
+// The most basic reasonable feature possible about a board: that a
+// particular spot has a particular color.
+type BasicFeature struct {
+	Color Color
+	Spot TopoSpot
+}
