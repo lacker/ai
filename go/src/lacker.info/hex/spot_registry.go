e98ed529b369ad8ef90d07413a878ce494c7d036
Kevin Lacker <lacker@gmail.com>
2014-11-17 18:53:23 -0800

    add sig of spot registry

--- go/src/lacker.info/hex/spot_registry.go
+++ go/src/lacker.info/hex/spot_registry.go
@@ -4,8 +4,25 @@ import (
 
 )
 
-// A SpotRegistry is designed to hold a bunch of listeners and notify
+// A SpotRegistry is designed to hold a bunch of listeners, each
+// affiliated to one spot, and notify
 // them when the contents of that spot change.
 type SpotRegistry struct {
+	// For now, all listeners are delta neurons, but this might be
+	// useful to extend.
+	// The first key is the spot. The second is the list of listeners.
+	listeners [NumTopoSpots][]*DeltaNeuron
+}
 
+// Create a new spot registry with no listeners
+func NewSpotRegistry() {
+	panic("TODO")
+}
+
+func (sr *SpotRegistry) Listen(spot TopoSpot, dn *DeltaNeuron) {
+	panic("TODO")
+}
+
+func (sr *SpotRegistry) Notify(bf BasicFeature) {
+	panic("TODO")
 }
