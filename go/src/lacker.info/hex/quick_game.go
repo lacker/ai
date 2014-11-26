54c432f4a1dd994602f00d721e5fd981b8bb3ba2
Kevin Lacker <lacker@gmail.com>
2014-11-25 18:19:01 -0800

    more renaming

--- go/src/lacker.info/hex/quick_game.go
+++ go/src/lacker.info/hex/quick_game.go
@@ -3,16 +3,16 @@ package hex
 import (
 )
 
-// A Playout object keeps around all the details of a single playout.
+// A QuickGame object keeps around all the details of a single playout.
 // This is kind of like an options object; there are just a lot of
 // different ways to handle a playout.
 
-type XPlayout struct {
+type QuickGame struct {
 	player1 QuickPlayer
 	player2 QuickPlayer
 	debug bool
 }
 
-func NewPlayout(p1 QuickPlayer, p2 QuickPlayer, debug bool) *XPlayout {
+func NewQuickGame(p1 QuickPlayer, p2 QuickPlayer, debug bool) *QuickGame {
 	panic("TODO")
 }
