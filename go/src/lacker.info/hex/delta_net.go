9705c6bbbe0689f2fe7e9c0566cdc2c6e497787d
Kevin Lacker <lacker@gmail.com>
2014-11-29 15:41:51 -0800

    evolution for defaults and the override spot

--- go/src/lacker.info/hex/delta_net.go
+++ go/src/lacker.info/hex/delta_net.go
@@ -9,7 +9,21 @@ type DeltaNet struct {
 	startingPosition *TopoBoard
 	color Color
 
-	neurons []DeltaNeuron
+	// This stores the neurons that just operate on a single basic feature
+	neurons map[BasicFeature]DeltaNeuron
+
+	// This stores the default scores for spots.
+	// This could be stored as a delta neuron with an empty input list,
+	// but this seems simpler.
+	defaultScores [NumTopoSpots]float64
+
+	// Always move to this spot if it's available.
+	// If this is NotASpot, ignore it.
+	// This is useful just to override the first move so that we don't
+	// overlearn it. It might help to expand this notion into a whole
+	// tree.
+	overrideSpot TopoSpot
+
 	spotPicker [NumTopoSpots]float64
 	registry *SpotRegistry
 }
@@ -18,14 +32,15 @@ func NewDeltaNet(board *TopoBoard, color Color) *DeltaNet {
 	return &DeltaNet{
 		startingPosition: board,
 		color: color,
-		neurons: make([]DeltaNeuron, 0),
+		neurons: make(map[BasicFeature]DeltaNeuron),
+		overrideSpot: NotASpot,
 		registry: NewSpotRegistry(),
 	}
 }
 
 func (net *DeltaNet) Reset() {
 	for i, _ := range net.spotPicker {
-		net.spotPicker[i] = 0.0
+		net.spotPicker[i] = net.defaultScores[i]
 	}
 	net.registry = NewSpotRegistry()
 
@@ -47,10 +62,14 @@ func (net *DeltaNet) Color() Color {
 
 func (net *DeltaNet) BestMove(board *TopoBoard, debug bool) (TopoSpot,
 	float64) {
+	if net.overrideSpot != NotASpot && board.Get(net.overrideSpot) != Empty {
+		return net.overrideSpot, 1337.0
+	}
+
 	bestSpot := NotASpot
 	bestScore := -1000000.0
 	for spot := TopLeftCorner; spot <= BottomRightCorner; spot++ {
-		if net.spotPicker[spot] > bestScore {
+		if net.spotPicker[spot] > bestScore && board.Get(spot) != Empty {
 			bestSpot = spot
 			bestScore = net.spotPicker[spot]
 		}
@@ -60,5 +79,22 @@ func (net *DeltaNet) BestMove(board *TopoBoard, debug bool) (TopoSpot,
 
 // The learning function
 func (net *DeltaNet) EvolveToPlay(ending *TopoBoard, debug bool) {
+	// The range of moves we'll be scanning over
+	begin := len(net.startingPosition.History)
+	end := len(ending.History)
+
+	// Improve default scores
+	for i := begin; i < end; i++ {
+		net.defaultScores[ending.History[i]] += 1.0
+	}
+
+	// Set the override spot
+	if net.startingPosition.GetToMove() == net.color {
+		net.overrideSpot = ending.History[begin]
+	} else {
+		net.overrideSpot = NotASpot
+	}
+
+	// Do neuronal learning
 	panic("TODO")
 }
