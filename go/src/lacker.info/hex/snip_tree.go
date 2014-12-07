ce7b3cd5129e00bf141925ae2aa6adb4ba7adff2
Kevin Lacker <lacker@gmail.com>
2014-12-07 13:28:42 -0800

    realize that snip trees do need to represent particular positions rather than just snip lists

--- go/src/lacker.info/hex/snip_tree.go
+++ go/src/lacker.info/hex/snip_tree.go
@@ -7,7 +7,8 @@ import (
 A SnipTree is used to solve the question of, given two evolving
 players, how can one alter its play to beat the second one?
 
-The SnipTree itself is a tree of ways to diverge from the main line.
+Each node in the SnipTree represents a position in the game, with the
+"evolving" player to move.
 
 The key heuristic, which is unfortunately specific to Hex, is that if
 there is a game tree in which an opponent always wins, but never
@@ -21,9 +22,10 @@ type SnipTree struct {
 	// The snips that define this tree node
 	snipList []Snip
 
-	// The node that is like this one, but does not have the last snip
-	// in snipList.
-	// We always investigate parents before children.
+	// The move index in the game of the next move to be made
+	index int
+
+	// The snip tree representing the game two ply before this one
 	parent *SnipTree
 
 	// TODO: track how many times particular spots are played
