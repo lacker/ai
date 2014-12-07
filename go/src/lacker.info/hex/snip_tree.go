ddd01a8c4c6866fbe7988965d3ecc03bbcc9d549
Kevin Lacker <lacker@gmail.com>
2014-12-07 13:47:45 -0800

    improve comments

--- go/src/lacker.info/hex/snip_tree.go
+++ go/src/lacker.info/hex/snip_tree.go
@@ -10,12 +10,10 @@ players, how can one alter its play to beat the second one?
 Each node in the SnipTree represents a position in the game, with the
 "evolving" player to move.
 
-The key heuristic, which is unfortunately specific to Hex, is that if
-there is a game tree in which an opponent always wins, but never
-plays a particular move, then making that move cannot possibly be part
-of a winning strategy for this player. Thus, we prioritize trying new
-moves according to how frequently the opponent uses them in the game
-tree descending from a particular position.
+The key principle, which is unfortunately specific to Hex, is that if
+an opponent can beat any move besides a move in the set S without ever
+moving in the set S, then we don't need to investigate moves in the
+set S to know that this position is lost.
 */
 
 type SnipTree struct {
