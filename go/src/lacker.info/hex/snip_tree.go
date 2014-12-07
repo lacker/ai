37af459cc1d991c9bacb7878e49df252e369f95b
Kevin Lacker <lacker@gmail.com>
2014-12-07 13:21:53 -0800

    more snip tree stuff

--- go/src/lacker.info/hex/snip_tree.go
+++ go/src/lacker.info/hex/snip_tree.go
@@ -18,5 +18,15 @@ tree descending from a particular position.
 */
 
 type SnipTree struct {
-	// TODO: implement
+	// The snips that define this tree node
+	snipList []Snip
+
+	// The node that is like this one, but does not have the last snip
+	// in snipList.
+	// We always investigate parents before children.
+	parent *SnipTree
+
+	// TODO: track how many times particular spots are played
+	// by the opponent in the tree rooted at this node?
+	// Depends on the specific algorithm we want.
 }
