36d23e6f58c61667afe69568467d1b425f5f7540
Kevin Lacker <lacker@gmail.com>
2014-12-31 14:05:26 -0800

    NewQPlayout

--- go/src/lacker.info/hex/q_playout.go
+++ go/src/lacker.info/hex/q_playout.go
@@ -23,5 +23,23 @@ func NewQPlayout(player1 *QNet, player2 *QNet) *QPlayout {
 		winner: Empty,
 	}
 
-	panic("TODO")
+	board := player1.StartingPosition().ToTopoBoard()
+	for board.Winner == Empty {
+		// player is the player whose move it is
+		var player *QNet
+		switch board.GetToMove() {
+		case player1.Color():
+			player = player1
+		case player2.Color():
+			player = player2
+		default:
+			panic("busted switch")
+		}
+
+		action := player.Act(board)
+		playout.actions = append(playout.actions, action)
+	}
+
+	playout.winner = board.Winner
+	return playout
 }
