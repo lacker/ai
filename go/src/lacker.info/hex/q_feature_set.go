c28f18dda9374bd26c80e27720f337a05e56844a
Kevin Lacker <lacker@gmail.com>
2015-01-03 12:09:37 -0800

    start q feature set, w\ file this time

---
+++ go/src/lacker.info/hex/q_feature_set.go
@@ -0,0 +1,20 @@
+package hex
+
+import (
+)
+
+// A compact representation of a small set of features.
+// This should be useful for things like indexing vectors over the
+// space of features.
+type QFeatureSet uint16
+
+// A QFeatureSet only handles up to 2 features.
+// 0: empty feature set
+// 1-242: single features (1 + qfeature)
+// some weird triangular thing: double features
+const EmptyFeatureSet QFeatureSet = 0
+const MinSingleFeature QFeatureSet = EmptyFeatureSet + 1
+const MaxSingleFeature QFeatureSet = MinSingleFeature +
+	QFeatureSet(MaxQFeature)
+
+
