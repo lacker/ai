851889632a1336865610260c41cdad899063a14e
Kevin Lacker <lacker@gmail.com>
2015-01-04 14:25:49 -0800

    feature set constants

--- go/src/lacker.info/hex/q_feature_set.go
+++ go/src/lacker.info/hex/q_feature_set.go
@@ -11,10 +11,21 @@ type QFeatureSet uint16
 // A QFeatureSet only handles up to 2 features.
 // 0: empty feature set
 // 1-242: single features (1 + qfeature)
-// some weird triangular thing: double features
+// 243-29403: double features
+//
+// The packing logic is basically, with features f1..f242:
+// 243: (f1, f2)
+// 244, 245: (f1, f3), (f2, f3)
+// 246, 247, 248: (f1, f4), (f2, f4), (f3, f4)
+// etc.
+// There are (242)(242-1)/2 = 29161 double features.
+
 const EmptyFeatureSet QFeatureSet = 0
-const MinSingleFeature QFeatureSet = EmptyFeatureSet + 1
-const MaxSingleFeature QFeatureSet = MinSingleFeature +
-	QFeatureSet(MaxQFeature)
+const MinSingleton QFeatureSet = EmptyFeatureSet + 1
+const NumSingletons = QFeatureSet(MaxQFeature)
+const MaxSingleton QFeatureSet = MinSingleton + NumSingletons - 1
+const MinDoubleton QFeatureSet = MaxSingleton + 1
+const NumDoubletons QFeatureSet = NumSingletons * (NumSingletons - 1) / 2
+const MaxDoubleton QFeatureSet = MinDoubleton + NumDoubletons - 1
 
 // TODO: handle double features
