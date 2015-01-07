0f35b1eccf8a71b524a154f7d345a5613fb77505
Kevin Lacker <lacker@gmail.com>
2015-01-07 14:05:22 -0800

    fix minor comment typo

--- go/src/lacker.info/hex/q_playout.go
+++ go/src/lacker.info/hex/q_playout.go
@@ -54,8 +54,25 @@ func NewQPlayout(player1 *QNet, player2 *QNet) *QPlayout {
 // Each playout defines a gradient, of the direction Q should go in
 // order to improve its accuracy according to Q-learning.
 // See q_net.go for details on the Q function.
-// AddGradient adds scalar times the gradient to addend.
-func (playout *QPlayout) AddGradient(scalar float64,
+//
+// In general, the gradient of the error on the logit can be
+// determined from the actual probabilities.
+// If the real probability was p_real and we predicted p, then the
+// gradient on the logit is just:
+// p_real - p
+// in every feature that the QNet sums to the logit for Q(s, a).
+//
+// To get the overall gradient for a whole playout, we need to do some
+// dynamic programming. The Q-learning rule defines one update that
+// can happen for each decision the provided color made. Since each
+// feature becomes active at a single point during the playout and
+// remains active for each successive Q-learning opportunity, we can
+// simultaneously apply each of the learning rules to each feature
+// using dynamic programming. See the code for more detail here.
+//
+// AddGradient adds scalar times the gradient to addend, using the
+// gradient for the provided color's decisions.
+func (playout *QPlayout) AddGradient(color Color, scalar float64,
 	addend *[NumFeatures]float64) {
 	panic("TODO")
 }
