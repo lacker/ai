55ac0120cefd833d0ae51b93f1c8fd8d61ed5808
Kevin Lacker <lacker@gmail.com>
2015-01-09 22:14:06 -0800

    more logistic testing

--- go/src/lacker.info/hex/q_net_test.go
+++ go/src/lacker.info/hex/q_net_test.go
@@ -14,4 +14,9 @@ func CheckApproxEq(x float64, y float64) {
 
 func TestLogisticMath(t *testing.T) {
 	CheckApproxEq(Logistic(0.0), 0.5)
+	probs := []float64{0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
+	for _, prob := range probs {
+		CheckApproxEq(Logistic(Logit(prob)), prob)
+	}
+		
 }
