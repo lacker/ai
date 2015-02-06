fa9905536c0ded7e00ed2a7ded9b19baa7b8757a
Kevin Lacker <lacker@gmail.com>
2015-02-05 23:25:40 -0800

    some basic math in lua working

--- lua/main.lua
+++ lua/main.lua
@@ -6,5 +6,15 @@ train = torch.load("mnist.t7/train_32x32.t7", "ascii")
 test = torch.load("mnist.t7/test_32x32.t7", "ascii")
 
 -- Usually for normalization, AI stuff uses zero mean and unit norm
+-- TODO: write helpers to do that.
 
--- TODO: tools for mean and norm
+
+-- Finds the global mean for all pixels
+function globalMean(tensor)
+  return tensor:sum() / tensor:nElement()
+end
+
+print(train.data:sum())
+print(train.data:nElement())
+
+print(globalMean(train.data))
