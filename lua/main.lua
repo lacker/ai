8a2810d9dfd356ca015b2e3e4383cd12108efe49
Kevin Lacker <lacker@gmail.com>
2015-02-06 18:43:37 -0800

    some matrix mathy stuff. slooow goin

--- lua/main.lua
+++ lua/main.lua
@@ -5,16 +5,29 @@ require "torch"
 train = torch.load("mnist.t7/train_32x32.t7", "ascii")
 test = torch.load("mnist.t7/test_32x32.t7", "ascii")
 
--- Usually for normalization, AI stuff uses zero mean and unit norm
--- TODO: write helpers to do that.
-
-
 -- Finds the global mean for all pixels
 function globalMean(tensor)
   return tensor:sum() / tensor:nElement()
 end
 
-print(train.data:sum())
-print(train.data:nElement())
+-- Normalizes data with the provided mean to have a unit norm.
+-- The first dimension of the tensor is assumed to index the different
+-- inputs for the data.
+function normalize(tensor, mean)
+  -- Apply the negative mean as an offset
+  normalized = torch.add(tensor, -mean)
 
-print(globalMean(train.data))
+  -- Square to start calculating norm
+  squares = torch.pow(normalized, 2)
+
+  -- Sum across the first index and square root to get norms
+  norms = TODO
+
+  -- Divide the original tensor elements by norms to normalize
+  -- TODO
+end
+
+
+mean = globalMean(train.data)
+
+print(mean)
