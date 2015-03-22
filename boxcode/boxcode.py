c8bd5ca77d3800c0257307d8257d62ee0219e8b3
Kevin Lacker <lacker@gmail.com>
2015-03-22 15:54:47 -0700

    simple lisp parser

--- boxcode/boxcode.py
+++ boxcode/boxcode.py
@@ -16,6 +16,30 @@ symbols are just represented as strings.
 numbers are represented as Python numbers.
 """
 def parse(serialized):
-  pass
+  tokens = serialized.replace("(", " ( ").replace(")", " ) ").split()
+
+  # Expressions that are partially created
+  answer = None
+  partials = [[]]
+
+  for token in tokens:
+    if answer is not None:
+      raise "oops too many tokens"
+      
+    if token == "(":
+      partials.append([])
+    elif token == ")":
+      expression = partials.pop()
+      partials[-1].append(expression)
+    else:
+      # It's an atom
+      if token[0] in ".0123456789":
+        partials[-1].append(float(token))
+      else:
+        partials[-1].append(token)
   
+  # partials[0][0] should be size 1 and have the answer
+  assert len(partials) == 1
+  assert len(partials[0]) == 1
+  return partials[0][0]
 
