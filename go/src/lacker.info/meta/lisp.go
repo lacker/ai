8565aa3a668d4b655ee2c9333173c417cd1f7278
Kevin Lacker <lacker@gmail.com>
2015-03-26 21:57:54 -0700

    initial meta stuff

---
+++ go/src/lacker.info/meta/lisp.go
@@ -0,0 +1,4 @@
+package meta
+
+// TODO: add things
+
---
+++ go/src/lacker.info/run_meta.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	// "lacker.info/meta"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) > 0 {
+		log.Fatal("run_meta takes no args")
+	}
+}
