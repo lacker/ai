774bdbefa8cae6b32fb28759050b4cb246b84d4a
Kevin Lacker <lacker@gmail.com>
2015-04-17 16:54:37 -0700

    poke around with beval

--- clojure/meta/src/meta/core.clj
+++ clojure/meta/src/meta/core.clj
@@ -1,6 +1,19 @@
 (ns meta.core
   (:gen-class))
 
+; Boson is a Lisp dialect that has eight core built-ins:
+; The "data structure" stuff: car, cdr, cons, nil
+; The "functional" stuff: lambda, recur, this
+; "if" because you need if
+
+(defn beval [expr]
+  "beval evaluates a list of Boson code."
+  (cons 'beval (cons expr nil)))
+
+(defn brepl []
+  "brepl runs a Boson repl."
+  nil)
+
 (defn -main
   "Let's build a Boson repl."
   [& args]
