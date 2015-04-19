27d0ad3de0cb040155ccfda85cebf936f6aa96a3
Kevin Lacker <lacker@gmail.com>
2015-04-19 13:57:44 -0700

    haskell comments

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -1,3 +1,7 @@
 #!/usr/bin/env runhaskell
 
 main = putStrLn "hello world!"
+
+-- What is the first part of Step that we could do?
+-- We could try to represent some basic stuff like enums, objects,
+-- functions.
