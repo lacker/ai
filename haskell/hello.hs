be43e3a6a994f115214a1e26be664299e87cb5e7
Kevin Lacker <lacker@gmail.com>
2015-04-20 20:46:28 -0700

    start of flip stuff

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -1,7 +1,3 @@
 #!/usr/bin/env runhaskell
 
 main = putStrLn "hello world!"
-
--- What is the first part of Step that we could do?
--- We could try to represent some basic stuff like enums, objects,
--- functions.
