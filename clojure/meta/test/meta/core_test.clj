12b5408a6dbc6ce6d6c559323ab8e4b01fbeeccb
Kevin Lacker <lacker@gmail.com>
2015-04-21 23:29:47 -0700

    test crossproduct

--- clojure/meta/test/meta/core_test.clj
+++ clojure/meta/test/meta/core_test.clj
@@ -52,4 +52,10 @@
     (is (thrown-with-msg? Exception #"recursive depth exceeded"
                           (beval '(loop (cons nil (cons nil nil)) nil))))
     )
+
+  (testing "cross-product"
+    (is (= [[1 3] [1 4] [2 3] [2 4]] (cross-product [1 2] [3 4])))
+    (is (= [[1 3 5] [1 3 6] [1 4 5] [1 4 6] [2 3 5] [2 3 6] [2 4 5] [2 4 6]]
+           (cross-product [1 2] [3 4] [5 6])))
+    )
   )
