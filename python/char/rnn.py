6c00df5045fdaa85ff6f6ac06c2e384c231b4c0d
Kevin Lacker <lacker@gmail.com>
2015-05-22 13:12:52 -0700

    make_labeled_data

--- python/char/rnn.py
+++ python/char/rnn.py
@@ -8,5 +8,22 @@ for inspiration.
 
 import os
 
+def make_labeled_data(chars):
+  """
+  Given a list of characters, turn it into labeled data.
+  That involves turning all characters into numbers.
+  In general the input vector is all characters leading up to a
+  certain point, and the target label is the next character.
+  """
+  dataset, labels = [], []
+  ints = map(ord, chars)
+  accum = []
+  for i in ints:
+    dataset.append(list(accum))
+    labels.append(i)
+    accum.append(i)
+  return dataset, labels
+  
+
 chatfile = os.path.realpath(__file__ + "/../data/chat.txt")
 print "chatfile:", chatfile
