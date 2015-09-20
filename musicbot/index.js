bc8da7402325aba55c54a6d96a7ef025146007d4
Kevin Lacker <lacker@gmail.com>
2015-09-19 17:37:02 -0700

    initial musicbot suggests one of two things

--- musicbot/index.js
+++ musicbot/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function choice(arr) {
+  if (arr.length < 1) {
+    throw "arr.length < 1";
+  }
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+app.get('/', function(req, res) {
+  res.send(choice([
+    "Smells Like Teen Spirit is great because it's such a classic.",
+    "Bangarang is cool because EDM is hot these days.",
+  ]));
 });
 
 var server = app.listen(3000, function () {
