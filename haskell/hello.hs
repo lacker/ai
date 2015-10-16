6919694ff703836f057f3d09e1a1a8920f3fb2d3
Kevin Lacker <lacker@gmail.com>
2015-10-15 21:53:26 -0700

    haskell merge fn

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -1,3 +1,10 @@
 #!/usr/bin/env runhaskell
 
-main = putStrLn "hello world!"
+-- Solving the interview problem of "print out only numbers that have
+-- only 2,3,5 as factors"
+
+-- merge two sorted lists to get another sorted list
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) = if x < y then x:(merge xs (y:ys)) else y:(merge (x:xs) ys)
