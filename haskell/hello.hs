6f38ca78f950be3520af27130bf04626a203e434
Kevin Lacker <lacker@gmail.com>
2015-10-16 16:01:55 -0700

    trimerge

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -8,3 +8,5 @@ merge :: (Ord a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys) = if x < y then x:(merge xs (y:ys)) else y:(merge (x:xs) ys)
+
+trimerge a b c = merge (merge a b) c
