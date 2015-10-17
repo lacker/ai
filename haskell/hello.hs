d1f1dada514886ecd8f8cb5fbfb7a768d8c56c94
Kevin Lacker <lacker@gmail.com>
2015-10-16 17:19:26 -0700

    solve ethe 235 problem

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -4,9 +4,12 @@
 -- only 2,3,5 as factors"
 
 -- merge two sorted lists to get another sorted list
+-- dedupes
 merge :: (Ord a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
-merge (x:xs) (y:ys) = if x < y then x:(merge xs (y:ys)) else y:(merge (x:xs) ys)
+merge (x:xs) (y:ys) = if x == y then x:(merge xs ys) else if x < y then x:(merge xs (y:ys)) else y:(merge (x:xs) ys)
 
 trimerge a b c = merge (merge a b) c
+
+foo = 1:(trimerge (map (* 2) foo) (map (* 3) foo) (map (* 5) foo))
