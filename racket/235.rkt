d4fc5d082df787123fdd3f914df3f5b90713cce6
Kevin Lacker <lacker@gmail.com>
2016-03-31 21:47:48 -0500

    more stream stuff

--- racket/235.rkt
+++ racket/235.rkt
@@ -6,14 +6,19 @@
     [(stream-empty? a) b]
     [(stream-empty? b) a]
     [(= (stream-first a) (stream-first b))
-     (cons (stream-first a) (merge (stream-rest a) (stream-rest b)))]
+     (stream-cons (stream-first a) (merge (stream-rest a) (stream-rest b)))]
     [(< (stream-first a) (stream-first b))
-     (cons (stream-first a) (merge (stream-rest a) b))]
-    [(cons (stream-first b) (merge a (stream-rest b)))]
+     (stream-cons (stream-first a) (merge (stream-rest a) b))]
+    [(stream-cons (stream-first b) (merge a (stream-rest b)))]
     ))
 
+(define (trimerge a b c) (merge (merge a b) c))
 
-
+(define (smult s k)
+  (cond
+    [(stream-empty? s) s]
+    [(stream-cons (* k (stream-first s)) (stream-rest s))]
+    ))
 
 (for ([x (merge '(1 2 3 5) '(2 4 5 6))])
   (writeln x))
