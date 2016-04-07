592f428bf2332804263f961ad52ced5822802fe4
Kevin Lacker <lacker@gmail.com>
2016-04-07 16:23:08 -0700

    euler 18

---
+++ racket/euler18.rkt
@@ -0,0 +1,39 @@
+#lang racket
+(require memoize)
+
+(define raw "75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23")
+
+(define table (for/list ([line (string-split raw "\n")])
+                (for/list ([part (string-split line " ")])
+                  (string->number part))))
+
+(define (get row col)
+  (list-ref (list-ref table row) col))
+
+(get 10 10)
+
+(define/memo (max-path-from row col)
+  (if (>= row (length table))
+      0
+      (+ (get row col)
+         (max (max-path-from (+ 1 row) (+ 1 col))
+              (max-path-from (+ 1 row) col))
+         )))
+
+(max-path-from 0 0)
+  
+            
