28bae9e4586cd5fae0a5a5edf5e20a7ffc46c5da
Kevin Lacker <lacker@gmail.com>
2016-11-28 21:27:41 -0800

    wrong answer grrr

--- python/94.py
+++ python/94.py
@@ -9,21 +9,15 @@ answer = 0
 # k has to be odd
 
 # Check k, k, k + u
-for k in range(3, billion, 2):
+for k in xrange(3, billion, 2):
   for u in (-1, +1):
     perimeter = 3 * k + u
     if perimeter > billion:
-      break
+      print 'answer:', answer
+      raise Exception('done')
     semi = perimeter / 2
-    area = sqrt(semi * (semi - k) * (semi - k) * (semi - k - u))
-    if int(area) == area:
+    # z = area / (semi - k) - heron's formula
+    z = sqrt(semi * (semi - k - u))
+    if int(z) == z:
       print k, k, k + u
       answer += perimeter
-  
-'''
-So semi * (semi - k - u) must be square
-== (3k + u) / 2 * (k - u) / 2 is square
-(3k + u) (k - u) / 4 is square
-'''
-
-print answer
