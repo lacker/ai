1c2019786edb01652a381551b6ed275e1ecf40bc
Kevin Lacker <lacker@gmail.com>
2016-12-06 17:34:12 -0800

    some databasey helper things

---
+++ js/site/database.js
@@ -0,0 +1,20 @@
+// The "database" is just a single object in memory.
+
+let database = {};
+
+export function all(tableName) {
+  if (!database[tableName]) {
+    database[tableName] = [];
+  }
+  return database[tableName];
+}
+
+export function push(tableName, data, limit) {
+  let table = all(tableName);
+  table.push(data);
+  if (limit) {
+    while (table.length > limit) {
+      table.shift();
+    }
+  }
+}
