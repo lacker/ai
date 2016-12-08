b48d79b1deadd9605a47c59a821edcc1a3c9ad7c
Kevin Lacker <lacker@gmail.com>
2016-12-07 17:54:02 -0800

    connect mock db to app root

--- js/site/database.js
+++ js/site/database.js
@@ -1,20 +1,20 @@
 // The "database" is just a single object in memory.
 
-let database = {};
+let database = {}
 
 export function all(tableName) {
   if (!database[tableName]) {
-    database[tableName] = [];
+    database[tableName] = []
   }
-  return database[tableName];
+  return database[tableName]
 }
 
 export function push(tableName, data, limit) {
-  let table = all(tableName);
-  table.push(data);
+  let table = all(tableName)
+  table.push(data)
   if (limit) {
     while (table.length > limit) {
-      table.shift();
+      table.shift()
     }
   }
 }
--- js/site/pages/index.js
+++ js/site/pages/index.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import database from './database'
 
-export default function Index() {
-  return (
-    <div>Yo this is the index</div>
-  )
+function makeID() {
+  let answer = '';
+  for (let i = 0; i < 8; i++) {
+    let index = Math.floor(Math.random() * 32
+    answer += 'ABCDEFGHJKMNPQRSTVWXYZ0123456789'[index]
+  }
+  return answer;
+}
+
+export default class extends React.Component {
+  static async getInitialProps({req}) {
+    let id = makeID();
+    database.push({
+      id,
+      content: 'this is message ' + id,
+    })
+
+    return {
+      messages: database.all('messages')
+    }
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.messages.map(message => {
+          <li key={message.id}>{message.content}</li>
+        })}
+      </ul>
+    )
+  }
 }
