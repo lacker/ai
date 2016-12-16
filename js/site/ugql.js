0750226db21814ae14ed394759a5c16997d3f7fd
Kevin Lacker <lacker@gmail.com>
2016-12-15 16:53:46 -0800

    simplify run

--- js/site/ugql.js
+++ js/site/ugql.js
@@ -29,27 +29,26 @@ function run(data, query) {
   let result = {}
   let promises = []
 
-  if (query.selectionSet) {
-    for (let field of query.selectionSet.selections) {
-      assert.equal(field.kind, 'Field')
-      let key = field.name.value;
-      if (field.selectionSet) {
-        promises.push(run(data[key], field).then(r => {
-          result[key] = r;
-        }))
-      } else if (field.arguments && field.arguments.length > 0) {
-        let args = {}
-        for (let arg of field.arguments) {
-          args[arg.name.value] = resolveValue(arg.value)
-        }
-        result[key] = data[key](args)
-      } else if (data[key].then) {
-        promises.push(data[key].then(val => {
-          result[key] = val;
-        }))
-      } else {
-        result[key] = data[key]
+  assert(query.selectionSet)
+  for (let field of query.selectionSet.selections) {
+    assert.equal(field.kind, 'Field')
+    let key = field.name.value;
+    if (field.selectionSet) {
+      promises.push(run(data[key], field).then(r => {
+        result[key] = r;
+      }))
+    } else if (field.arguments && field.arguments.length > 0) {
+      let args = {}
+      for (let arg of field.arguments) {
+        args[arg.name.value] = resolveValue(arg.value)
       }
+      result[key] = data[key](args)
+    } else if (data[key].then) {
+      promises.push(data[key].then(val => {
+        result[key] = val;
+      }))
+    } else {
+      result[key] = data[key]
     }
   }
 
