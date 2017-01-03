e8a837298c4e3017bb8c20d38b581761b20b6547
Kevin Lacker <lacker@gmail.com>
2017-01-02 19:46:15 -0800

    can't get things to work with mobx plus next

--- js/site/pages/index.js
+++ js/site/pages/index.js
@@ -1,73 +1,24 @@
 import React from 'react';
-import { extendObservable } from 'mobx';
-import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 import 'isomorphic-fetch';
 
-import ChatInput from '../ChatInput';
+import ListView from '../ListView';
 
-function makeID() {
-  let answer = '';
-  for (let i = 0; i < 8; i++) {
-    let index = Math.floor(Math.random() * 32);
-    answer += 'ABCDEFGHJKMNPQRSTVWXYZ0123456789'[index];
-  }
-  return answer;
-}
-
-let store = {};
-extendObservable(store, {
-  messages: [],
-});
-
-class ListView extends React.Component {
+class App extends React.Component {
   static async getInitialProps({req}) {
 
     const res = await fetch('http://localhost:2428/messages');
     const data = await res.json();
-    store.messages = data;
+
+    let messages = observable(data);
     return {
-      messages: store.messages,
+      messages: messages,
     };
   }
 
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(value) {
-    console.log(
-      'at the start of handleSubmit messages is:',
-      JSON.stringify(this.props.messages));
-
-    // TODO: use chat server instead of just locally doing stuff
-    // TODO: figure out how to make this rerender the ListView
-    this.props.messages.push({
-      id: makeID(),
-      content: value,
-    });
-    
-    console.log(
-      'at the end of handleSubmit messages is:',
-      JSON.stringify(this.props.messages));
-  }
-
   render() {
-    console.log('rendering messages:', JSON.stringify(this.props.messages));
-    return (
-      <div>
-      Welcome to chat.
-        <ul>
-          {this.props.messages.map(message => (
-            <li key={message.id}>{message.content}</li>
-          ))}
-        </ul>
-        <ChatInput onSubmit={this.handleSubmit} />
-      </div>
-    );
+    return <ListView messages={this.props.messages} />;
   }
 }
-ListView = observer(ListView);
 
-export default ListView;
+export default App;
