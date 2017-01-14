7344aeedf61e9ea34b595a5a5ca261f11fbf6b2d
Kevin Lacker <lacker@gmail.com>
2017-01-14 08:50:05 -0800

    refactor out a merge function

--- js/web/src/MessageClient.js
+++ js/web/src/MessageClient.js
@@ -16,20 +16,25 @@ export default class MessageClient {
     this.messages = observable([]);
   }
 
+  // Merges in a list of new messages
+  merge(data) {
+    let newMessages = {};
+    for (let message of this.messages) {
+      newMessages[message.id] = message;
+    }
+    for (let message of data) {
+      newMessages[message.id] = message;
+    }
+    newMessages.sort((m1, m2) => (m1.timestamp - m2.timestamp));
+    this.messages.replace(newMessages);
+    return this.messages;
+  }
+
   load() {
     return fetch('http://localhost:2428/messages').then(res => {
       return res.json();
     }).then(data => {
-      let newMessages = {};
-      for (let message of this.messages) {
-        newMessages[message.id] = message;
-      }
-      for (let message of data) {
-        newMessages[message.id] = message;
-      }
-      newMessages.sort((m1, m2) => (m1.timestamp - m2.timestamp));
-      this.messages.replace(newMessages);
-      return this.messages;
+      return this.merge(data);
     });
   }
 
